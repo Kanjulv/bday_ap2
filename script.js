705ca9d78bdb49c5fe04db934edfb76311a4ba7f
Kanjulv <118467727+Kanjulv@users.noreply.github.com>
2024-01-13 23:26:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function nextPage() {
+    window.location.href = "yes.html";
+}
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth);
+    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight);
+    document.getElementById('noButton').style.left = `${x}px`;
+    document.getElementById('noButton').style.top = `${y}px`;
+}
+
+console.log("Made with love by - Kanjul Verma ❣️");
+
+
+// function askName() {
+//     var userName;
+
+//     // Keep prompting until a valid name is provided
+//     while (!userName) {
+//         userName = prompt("What is your name?");
+//     }
+
+//     // Store the user's name in localStorage for later use
+//     localStorage.setItem('userName', userName);
+// }
+
+// for space 
+// function askName() {
+//     var userName;
+
+//     // Keep prompting until a valid name is provided
+//     while (!userName || /\s/.test(userName)) {
+//         userName = prompt("What is your name? Please enter a name without spaces.");
+//     }
+
+//     // Store the user's name in localStorage for later use
+//     localStorage.setItem('userName', userName);
+// }
---
+++ yes_script.js
@@ -0,0 +1,15 @@
+console.log("Made with love by - Kanjul  ❣️");
+
+
+// Function to display the user's name on the second page
+function displayUserName() {
+    // Retrieve the user's name from localStorage
+
+    // Display the user's name in the h1 tag
+    // if (userName) {
+        document.getElementById('userNameDisplay').innerText = "YAYYYEEEEE!!!!!!!!!!! Let's go Ap!! " ;
+//     } else {
+//         // Handle the case where the user's name is not available
+//         document.getElementById('userNameDisplay').innerText = "YAYYYEEEEE!!!!!!!!!!! ";
+//     }
+}
